71a18640dd186630d6194fb44c513a32f4b811e9
Mrinmoy1024 <mtex1024@gmail.com>
2026-04-07 09:16:13 +0600

    context added

---
+++ src/pages/Contests/Contests.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Contests = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Contests;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/home/Home";
+import Contests from "./pages/Contests/Contests";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
     children: [
-        {
-            index: true,
-            element: <Home></Home>
-        },
-        {
-        }
-    ]
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        index: "/contests",
+        element: <Contests></Contests>,
+      },
+    ],
   },
 ]);
 
