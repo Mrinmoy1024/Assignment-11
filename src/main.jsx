3de1f06b665ca0f7db712acce63b0b08527d7e59
Mrinmoy1024 <mtex1024@gmail.com>
2026-04-11 12:16:49 +0600

    loading spinner issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider } from "react-router";
 import AuthProvider from "./context/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
